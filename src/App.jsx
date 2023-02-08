34cac3335d14c4ee8c0edc016c20df7acd441980
mustafa <mustafanaiby@gmail.com>
2023-02-08 07:57:00 +0330

    Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import "./App.css";
 import Home from "./Components/home/home";
 import Project from "./Components/project/project";
-import Service from "./Components/service/service";
+
 function App() {
   return (
     <>
       <Home />
       <Project />
-      {/* <Service /> */}
     </>
   );
 }
